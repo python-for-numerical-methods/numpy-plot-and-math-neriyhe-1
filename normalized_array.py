8a43ab3e1ef5cc014df6d95f0889d685a2e3b28f
neriyhe <neriyhe@ac.sce.ac.il>
2026-05-10 15:52:34 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -8,9 +8,3 @@ def normalized_array(data):
     new_arr = (data_arr - np.min(data_arr))/(np.max(data_arr) - np.min(data_arr))
 
   return new_arr
-
-if __name__ == "__main__":
-    # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
-    test_data = [10, 20, 30, 40, 50]
-    print(f"Original: {test_data}")
-    print(f"Normalized: {normalized_array(test_data)}")
